cd862f48b6021371272907f3120bc0a0e07e1a51
yishuzhang0302 <yz923@cornell.edu>
2018-05-16 14:35:21 -0400

    update stock.java

--- caseStudy/services/src/main/java/pojo/Stock.java
+++ caseStudy/services/src/main/java/pojo/Stock.java
@@ -25,6 +25,29 @@ public class Stock {
     // TODO - Think back to your modelling session
     // Define the attributes of a stock price based on the
     // provided data in resources/data
+    private String name;
+    private HashTree<String, Integer> dailyClosePrice;
+
+    public Stock(String name, HashTree<String, Integer> prices) {
+        this.name = name;
+        dailyClosePrice = prices;
+    } 
 
     // TODO - add getter and setter methods for your attributes
+
+    public String get_name() {
+        return this.name;
+    }
+
+    public HashTree<String, Integer> get_prices() {
+        return this.dailyClosePrice;
+    }
+
+    public void set_name(String name) {
+        this.name = name;
+    }
+
+    public void set_prices(HashTree<String, Integer> prices) {
+        dailyClosePrice = prices;
+    }
 }
